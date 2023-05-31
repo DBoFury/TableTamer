f82b0689b30993755198107bce73e7a72cb89513
Олег Дидечкин <crm.29431@dz.com>
2023-05-31 13:11:24 +0300

    Styling fixing

--- frontend/src/components/OrdersPaginate/OrdersPaginate.tsx
+++ frontend/src/components/OrdersPaginate/OrdersPaginate.tsx
@@ -1,5 +1,6 @@
 import ReactPaginate from "react-paginate";
 import { IoArrowForward, IoArrowBack } from "react-icons/io5";
+import StickyWrapper from "../StickyWrapper/StickyWrapper";
 import "./OrdersPaginate.css";
 
 interface OrdersPaginatePropsType {
@@ -12,22 +13,24 @@ const OrdersPaginate = ({
   handlePageClick,
 }: OrdersPaginatePropsType) => {
   return (
-    <ReactPaginate
-      activeClassName={"item active"}
-      breakClassName={"item break-me"}
-      breakLabel={"..."}
-      containerClassName={"pagination"}
-      disabledClassName={"disabled-page"}
-      marginPagesDisplayed={2}
-      nextClassName={"item next"}
-      nextLabel={<IoArrowForward style={{ fontSize: 30, width: 50 }} />}
-      onPageChange={handlePageClick}
-      pageCount={pageCount}
-      pageClassName={"item pagination-page"}
-      pageRangeDisplayed={2}
-      previousClassName={"item previous"}
-      previousLabel={<IoArrowBack style={{ fontSize: 30, width: 50 }} />}
-    />
+    <StickyWrapper>
+      <ReactPaginate
+        activeClassName={"item active"}
+        breakClassName={"item break-me"}
+        breakLabel={"..."}
+        containerClassName={"pagination"}
+        disabledClassName={"disabled-page"}
+        marginPagesDisplayed={2}
+        nextClassName={"item next"}
+        nextLabel={<IoArrowForward style={{ fontSize: 30, width: 50 }} />}
+        onPageChange={handlePageClick}
+        pageCount={pageCount}
+        pageClassName={"item pagination-page"}
+        pageRangeDisplayed={2}
+        previousClassName={"item previous"}
+        previousLabel={<IoArrowBack style={{ fontSize: 30, width: 50 }} />}
+      />
+    </StickyWrapper>
   );
 };
 
